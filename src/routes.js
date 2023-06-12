cb67c5ae225636e129fcbb79411f6e0579d07ebf
AlipioGSF <alipiotec@gmail.com>
2023-06-12 20:59:41 -0300

    Adicionado: Inicio painel do usuário

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import { lazy } from 'react'
 
 const Main = lazy(() => import('./views/main/Main'))
 const Cadastro = lazy(()=> import('./views/Cadastro/Cadastro'))
+const Painel = lazy(()=> import('./views/Painel/Painel'));
 
 export const routes = [
   { path: '/', exact: true, name: 'home' },
   { path: '/main', name: 'Main', element: Main },
   { path: '/cadastro', name: 'Cadastro', element: Cadastro},
+  { path: '/painel', name: 'Painel', element: Painel}
 ]
---
+++ src/views/Painel/Painel.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+
+const Painel = () => {
+
+  return (
+    <main>
+        <aside>
+            <ul>
+                <li><button>Meus Pedidos</button></li>
+                <li><button>Fazer Pedido</button></li>
+            </ul>
+        </aside>
+    </main>
+
+
+  )
+}
+
+export default Painel
